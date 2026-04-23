d43b3b11ce69c6735a953a847093d3208a6d1fba
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-23 11:14:35 +0530

    undergraduate food corner stall

---
+++ src/main/java/com/undergraduate/cloud_food_corner/CloudFoodCornerApplication.java
@@ -0,0 +1,13 @@
+package com.undergraduate.cloud_food_corner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFoodCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudFoodCornerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/undergraduate/cloud_food_corner/CloudFoodCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undergraduate.cloud_food_corner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudFoodCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
