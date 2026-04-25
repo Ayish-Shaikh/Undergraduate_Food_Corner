32c1f07c22ad1d7c1130f301d7952007586490d5
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-25 23:53:55 +0530

    feature: Taking a request from the browser Preparing menu data Sending it to the view (menu.html)

---
+++ src/main/java/com/undergraduate/cloud_food_corner/controller/MenuController.java
@@ -0,0 +1,10 @@
+package com.undergraduate.cloud_food_corner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/menu")
+public class MenuController {
+
+}
