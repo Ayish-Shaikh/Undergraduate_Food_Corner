8b4103266ca3c8538898801edc37b61477fc2967
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-25 23:34:51 +0530

    add: MenuItem for my menu future: add db

---
+++ src/main/java/com/undergraduate/cloud_food_corner/model/MenuItem.java
@@ -0,0 +1,69 @@
+package com.undergraduate.cloud_food_corner.model;
+
+public class MenuItem {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private Category category;
+    private String imageUrl;
+
+    public MenuItem(Long id, String name, String description,
+                    double price, Category category,
+                    String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
