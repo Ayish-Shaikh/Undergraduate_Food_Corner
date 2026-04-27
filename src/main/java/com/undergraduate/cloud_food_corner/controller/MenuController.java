f137b8ab7a0de029ebe5d16560dafb03d3766962
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-27 21:23:30 +0530

    fix: images ref added to menu controller when a user is calling the /api/menu

--- src/main/java/com/undergraduate/cloud_food_corner/controller/MenuController.java
+++ src/main/java/com/undergraduate/cloud_food_corner/controller/MenuController.java
@@ -17,11 +17,11 @@ public class MenuController {
         return Arrays.asList(
 
                 new MenuItem(1L, "Ilish Bhapa", "Steamed hilsa in mustard",
-                        220.0, Category.MAIN_COURSE, "/images/ilish.png"),
+                        220.0, Category.MAIN_COURSE, "/images/ilishbhapa.png"),
                 new MenuItem(2L, "Kosha Mangsho", "Slow cooked mutton curry",
-                        280.0, Category.MAIN_COURSE, "/images/mangsho.png"),
+                        280.0, Category.MAIN_COURSE, "/images/koshamangsho.png"),
                 new MenuItem(3L, "Bhoger Khichuri", "Rice and lentil Comfort bowl",
-                        120.0, Category.MAIN_COURSE, "/images/khichuri.png"),
+                        120.0, Category.MAIN_COURSE, "/images/khichudi.png"),
 
                 new MenuItem(4L, "Phuchka", "Tamarind water crispy puri",
                         60.0, Category.STREET_FOOD, "/images/phuchka.png"),
@@ -31,16 +31,16 @@ public class MenuController {
                         50.0, Category.STREET_FOOD, "/images/jhalmuri.png"),
 
                 new MenuItem(7L, "Mishti Doi", "Sweet fermented yoghurt",
-                        70.0, Category.SWEETS, "/images/mishtidoi.png"),
+                        70.0, Category.SWEETS, "/images/mistidoi.png"),
                 new MenuItem(8L, "Sandesh", "Fresh chhena sweet",
                         90.0, Category.SWEETS, "/images/sandesh.png"),
                 new MenuItem(9L, "Rasgulla", "Soft spongy chhena balls",
-                        80.0, Category.SWEETS, "/images/rasgulla.png"),
+                        80.0, Category.SWEETS, "/images/rosogolla.png"),
 
                 new MenuItem(10L, "Aam Panna", "Raw mango cooler",
-                        60.0, Category.BEVERAGES, "/images/aampanna.png"),
+                        60.0, Category.BEVERAGES, null),
                 new MenuItem(11L, "Lassi", "Sweet yoghurt drink",
-                        70.0, Category.BEVERAGES, "/images/lassi.png")
+                        70.0, Category.BEVERAGES, null)
         );
     }
     @GetMapping
