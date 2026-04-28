b32f6af0f5a5eff25b6b1926626e11b7a1977604
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-04-28 22:46:24 +0530

    add: business logic for the controller class and asks for the order details

--- src/main/java/com/undergraduate/cloud_food_corner/model/Order.java
+++ src/main/java/com/undergraduate/cloud_food_corner/model/Order.java
@@ -1,4 +1,48 @@
 package com.undergraduate.cloud_food_corner.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    private String customerName;
+    private String deliveryAddress;
+    private String phone;
+    private List<MenuItem> items = new ArrayList<>();
+
+    public void addItem(MenuItem item){
+        this.items.add(item);
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<MenuItem> items) {
+        this.items = items;
+    }
 }
