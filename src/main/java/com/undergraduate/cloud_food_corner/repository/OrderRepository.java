1226ab7a0d30c9d33caf7018f1fd86dd22d856a3
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-05-01 23:54:40 +0530

    Order repo logic added

--- src/main/java/com/undergraduate/cloud_food_corner/repository/OrderRepository.java
+++ src/main/java/com/undergraduate/cloud_food_corner/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.undergraduate.cloud_food_corner.repository;
 
-public class OrderRepository {
+import com.undergraduate.cloud_food_corner.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
