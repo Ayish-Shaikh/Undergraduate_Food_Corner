ba0249fc2b3dc5caa18142769a98d01eac643f55
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-05-01 23:54:13 +0530

    menu item repo logic added

--- src/main/java/com/undergraduate/cloud_food_corner/repository/MenuItemRepository.java
+++ src/main/java/com/undergraduate/cloud_food_corner/repository/MenuItemRepository.java
@@ -1,4 +1,9 @@
 package com.undergraduate.cloud_food_corner.repository;
 
-public class MenuItemRepository {
+import com.undergraduate.cloud_food_corner.model.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 }
