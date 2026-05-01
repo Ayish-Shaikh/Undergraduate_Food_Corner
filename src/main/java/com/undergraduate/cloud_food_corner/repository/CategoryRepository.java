308edf4f28de621303c0eaa184d6309d098063da
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-05-01 23:53:11 +0530

    add: category repo logic written

--- src/main/java/com/undergraduate/cloud_food_corner/repository/CategoryRepository.java
+++ src/main/java/com/undergraduate/cloud_food_corner/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.undergraduate.cloud_food_corner.repository;
 
-public class CategoryRepository {
+import com.undergraduate.cloud_food_corner.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
