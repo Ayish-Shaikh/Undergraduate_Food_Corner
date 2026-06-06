5d8cf28f74465b7ae682e2655f952ee93cd4c6f4
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-06-06 23:09:41 +0530

    feat: Add User entity implementing UserDetails interface

--- src/main/java/com/undergraduate/cloud_food_corner/model/User.java
+++ src/main/java/com/undergraduate/cloud_food_corner/model/User.java
@@ -1,4 +1,50 @@
 package com.undergraduate.cloud_food_corner.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private final String username;
+    private final String password;
+    private final String fullname;
+    private final String street;
+    private final String city;
+    private final String state;
+    private final String zip;
+    private final String phoneNumber;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
