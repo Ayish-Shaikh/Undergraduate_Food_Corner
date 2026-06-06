38f297fddcf44a5c1ff88588c24092b6a98c1393
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-06-06 23:02:58 +0530

    feat: Add UserRepository with findByUsername method

--- src/main/java/com/undergraduate/cloud_food_corner/repository/UserRepository.java
+++ src/main/java/com/undergraduate/cloud_food_corner/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.undergraduate.cloud_food_corner.repository;
 
-public class UserRepository {
+import com.undergraduate.cloud_food_corner.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
