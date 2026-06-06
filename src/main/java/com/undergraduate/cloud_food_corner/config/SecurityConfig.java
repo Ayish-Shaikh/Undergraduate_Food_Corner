de9dfed65b2dd7df7b29620f22b9e266811b0c83
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2026-06-06 23:01:02 +0530

    feat: Add Spring Security config with form login

--- src/main/java/com/undergraduate/cloud_food_corner/config/SecurityConfig.java
+++ src/main/java/com/undergraduate/cloud_food_corner/config/SecurityConfig.java
@@ -1,23 +1,35 @@
 package com.undergraduate.cloud_food_corner.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(authz -> authz
-                .requestMatchers("/", "/menu", "/images/**", "/h2-console/**").permitAll()
+                .requestMatchers("/", "/menu", "/images/**","/register","/h2-console/**").permitAll()
                 .anyRequest().authenticated()
         )
-                .formLogin(form -> form.permitAll())
-                .csrf(csrf -> csrf.disable())
-                .headers(headers ->
-                        headers.frameOptions(frame -> frame.disable()));
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                );
         return http.build();
     }
 }
---
+++ src/main/java/com/undergraduate/cloud_food_corner/controller/RegistrationController.java
@@ -0,0 +1,4 @@
+package com.undergraduate.cloud_food_corner.controller;
+
+public class RegistrationController {
+}
